f4568d71c2f613e92eea3179f342c7ad956237b3
misaki.otb <83867846+misakiotb@users.noreply.github.com>
2024-10-24 00:36:49 +0900

    複数人対応、スタイル調整

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import {
 } from "aws-sdk/clients/rekognition";
 import AWS from "aws-sdk";
 import { Buffer } from 'buffer';
+import { Collapse } from '@material-ui/core';
 
 // @ts-ignore
 window.Buffer = Buffer;
@@ -27,6 +28,21 @@ const useStyles = makeStyles(() => ({
     width: "100%",
     flexDirection: "row",
     justifyContent: "flex-start",
+  },
+  resulttable:{
+    margin: "20px",
+    borderCollapse: "collapse",
+    border: "5px solid #ddd",
+  },
+  resulttableth:{
+    padding: "8px",
+    textAlign: "center",
+    border: "2px solid #ddd",
+  },
+  resulttabletd:{
+    padding: "8px",
+    textAlign: "center",
+    border: "2px solid #ddd",
   },
 }));
 
@@ -63,43 +79,45 @@ const detectFaces = async (imageData: string): Promise<DetectFacesResponse> => {
 };
 
 //分析結果からConfidence（分析結果の信頼度）取得
-const getConfidence = (rekognizeResult: DetectFacesResponse): number => {
-  if ((rekognizeResult.FaceDetails as FaceDetailList)[0]) {
-    return (rekognizeResult.FaceDetails as FaceDetailList)[0].Confidence!;
+const getConfidence = (rekognizeResult: DetectFacesResponse, num: number): number => {
+  if ((rekognizeResult.FaceDetails as FaceDetailList)[num]) {
+    return Number((rekognizeResult.FaceDetails as FaceDetailList)[num].Confidence!.toFixed(1));
   } else {
     return 0;
   }
 };
 
 //分析結果からSmile（笑っているか）取得
-const getIsSmiling = (rekognizeResult: DetectFacesResponse): string => {
-  if ((rekognizeResult.FaceDetails as FaceDetailList)[0]) {
-      const isSmile = (rekognizeResult.FaceDetails as FaceDetailList)[0].Smile?.Value!;
+const getIsSmiling = (rekognizeResult: DetectFacesResponse, num: number): string => {
+  if ((rekognizeResult.FaceDetails as FaceDetailList)[num]) {
+      const isSmile = (rekognizeResult.FaceDetails as FaceDetailList)[num].Smile?.Value!;
     if (isSmile) {
-      return "ナイス　スマイル！";
+      return "☺ナイス スマイル☺";
     } else {
-      return "ノー　スマイル";
+      return "スマイルがたりないよ！";
     }
   } else {
-    return "";
+    return "－";
   }
 };
 
 
 //分析結果からSmile（笑っているか）の信頼度を取得
-const getIsSmilingConfidence = (rekognizeResult: DetectFacesResponse): number => {
-  if ((rekognizeResult.FaceDetails as FaceDetailList)[0]) {
-    return (rekognizeResult.FaceDetails as FaceDetailList)[0].Smile?.Confidence!;
-  } else {
-    return 0;
+const getIsSmilingConfidence = (rekognizeResult: DetectFacesResponse, num: number): number => {
+  if ((rekognizeResult.FaceDetails as FaceDetailList)[num]) {
+    const isSmile = (rekognizeResult.FaceDetails as FaceDetailList)[num].Smile?.Value!;
+    if (isSmile) {
+      return Number((rekognizeResult.FaceDetails as FaceDetailList)[num].Smile?.Confidence!.toFixed(1));
+    }
   }
+  return 0;
 };
 
 
 //分析結果から感情を推定
-const getEmotion = (rekognizeResult: DetectFacesResponse): string => {
-  if ((rekognizeResult.FaceDetails as FaceDetailList)[0]) {
-    const emotions = (rekognizeResult.FaceDetails as FaceDetailList)[0]
+const getEmotion = (rekognizeResult: DetectFacesResponse, num: number): string => {
+  if ((rekognizeResult.FaceDetails as FaceDetailList)[num]) {
+    const emotions = (rekognizeResult.FaceDetails as FaceDetailList)[num]
     .Emotions as Emotions;
     const largestEmotionConfidence = Math.max(
       ...(emotions.map((d) => d.Confidence) as number[])
@@ -138,7 +156,8 @@ function App() {
   return (
     <>
       <header>
-        <h1>スマイルチェッカー:SMILE CHECKER</h1>
+        <h1>スマイルチェッカー Smile Checker v1.0</h1>
+        <h3>～みんな えがおに なれるかな？～</h3>
       </header>
       {isCaptureEnable || (
         <button onClick={() => setCaptureEnable(true)}>カメラ ON</button>
@@ -180,38 +199,46 @@ function App() {
           </div>
           {typeof rekognizeResult !== "undefined" && (
             <div className={classes.rekognizeResult}>
-              <table>
+              <table className={classes.resulttable}>
                 <tr>
-                  <th>No.</th>
-                  <th>スマイル</th>
-                  <th>スマイルポイント</th>
-                  <th>感情</th>
-                  <th>分析の信頼度</th>
+                  <th className={classes.resulttableth}>No.</th>
+                  <th className={classes.resulttableth}>スマイル</th>
+                  <th className={classes.resulttableth}>スマイルポイント</th>
+                  <th className={classes.resulttableth}>感情</th>
                 </tr>
                 <tr>
-                  <td>1人目</td>
-                  <td>{getIsSmiling(rekognizeResult)}</td>
-                  <td>{getIsSmilingConfidence(rekognizeResult)}</td>
-                  <td>{getEmotion(rekognizeResult)}</td>
-                  <td>{getConfidence(rekognizeResult)}</td>
+                  <td className={classes.resulttabletd}>1人目</td>
+                  <td className={classes.resulttabletd}>{getIsSmiling(rekognizeResult,0)}</td>
+                  <td className={classes.resulttabletd}>{getIsSmilingConfidence(rekognizeResult,0)}</td>
+                  <td className={classes.resulttabletd}>{getEmotion(rekognizeResult,0)}</td>
                 </tr>
                 <tr>
-                  <td>2人目</td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
+                  <td className={classes.resulttabletd}>2人目</td>
+                  <td className={classes.resulttabletd}>{getIsSmiling(rekognizeResult,1)}</td>
+                  <td className={classes.resulttabletd}>{getIsSmilingConfidence(rekognizeResult,1)}</td>
+                  <td className={classes.resulttabletd}>{getEmotion(rekognizeResult,1)}</td>
                 </tr>
                 <tr>
-                  <td>3人目</td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
+                  <td className={classes.resulttabletd}>3人目</td>
+                  <td className={classes.resulttabletd}>{getIsSmiling(rekognizeResult,2)}</td>
+                  <td className={classes.resulttabletd}>{getIsSmilingConfidence(rekognizeResult,2)}</td>
+                  <td className={classes.resulttabletd}>{getEmotion(rekognizeResult,2)}</td>
+                </tr>
+                <tr>
+                  <td className={classes.resulttabletd}>4人目</td>
+                  <td className={classes.resulttabletd}>{getIsSmiling(rekognizeResult,3)}</td>
+                  <td className={classes.resulttabletd}>{getIsSmilingConfidence(rekognizeResult,3)}</td>
+                  <td className={classes.resulttabletd}>{getEmotion(rekognizeResult,3)}</td>
+                </tr>
+                <tr>
+                  <td className={classes.resulttabletd}>5人目</td>
+                  <td className={classes.resulttabletd}>{getIsSmiling(rekognizeResult,4)}</td>
+                  <td className={classes.resulttabletd}>{getIsSmilingConfidence(rekognizeResult,4)}</td>
+                  <td className={classes.resulttabletd}>{getEmotion(rekognizeResult,4)}</td>
                 </tr>
               </table>
               <div>
-                <h3>感情の種類</h3>
+                <h3>感情分析の種類</h3>
                 <div>
                   <ul>
                     <li>HAPPY : しあわせ</li>
@@ -223,6 +250,7 @@ function App() {
                     <li>FEAR : こわい</li>
                     <li>SAD : かなしい</li>
                   </ul>
+                  <div>AI（えーあい）が分析した感情はどうだったかな？</div>
                 </div>
               </div>
             </div>
